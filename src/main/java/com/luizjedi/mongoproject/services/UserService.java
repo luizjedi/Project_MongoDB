8b7d8e3ccca60b5b2a6deff23ddb42cae2791385
luizjedi <luiz.pepe@hotmail.com>
2021-05-21 19:28:21 -0300

    Insertion of user with POST

--- src/main/java/com/luizjedi/mongoproject/services/UserService.java
+++ src/main/java/com/luizjedi/mongoproject/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luizjedi.mongoproject.domain.User;
+import com.luizjedi.mongoproject.dto.UserDTO;
 import com.luizjedi.mongoproject.repository.UserRepository;
 import com.luizjedi.mongoproject.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 		
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
 }
