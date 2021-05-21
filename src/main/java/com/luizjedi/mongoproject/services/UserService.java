ee5db47cffefc3d75691675cfa5c9631b5d9509a
luizjedi <luiz.pepe@hotmail.com>
2021-05-21 19:13:29 -0300

    Getting user for id

--- src/main/java/com/luizjedi/mongoproject/services/UserService.java
+++ src/main/java/com/luizjedi/mongoproject/services/UserService.java
@@ -1,12 +1,14 @@
 package com.luizjedi.mongoproject.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luizjedi.mongoproject.domain.User;
 import com.luizjedi.mongoproject.repository.UserRepository;
+import com.luizjedi.mongoproject.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
