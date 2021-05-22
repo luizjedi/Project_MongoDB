d70bcbd64aaf851e36b80cdf06b806786ba5521a
luizjedi <luiz.pepe@hotmail.com>
2021-05-21 21:12:31 -0300

    Endpoint for return the posts user

--- src/main/java/com/luizjedi/mongoproject/resources/UserResources.java
+++ src/main/java/com/luizjedi/mongoproject/resources/UserResources.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.luizjedi.mongoproject.domain.Post;
 import com.luizjedi.mongoproject.domain.User;
 import com.luizjedi.mongoproject.dto.UserDTO;
 import com.luizjedi.mongoproject.services.UserService;
@@ -65,5 +66,13 @@ public class UserResources {
 		user.setId(id);
 		user = service.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+
+		User user = service.findById(id);
+
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 }
