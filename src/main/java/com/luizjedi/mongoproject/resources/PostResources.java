4b57e7e1c1fe9dd3583a1ac6f7a48f0bbc93f11f
luizjedi <luiz.pepe@hotmail.com>
2021-05-21 21:57:51 -0300

    Simple consult with query methods

--- src/main/java/com/luizjedi/mongoproject/resources/PostResources.java
+++ src/main/java/com/luizjedi/mongoproject/resources/PostResources.java
@@ -1,13 +1,17 @@
 package com.luizjedi.mongoproject.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.luizjedi.mongoproject.domain.Post;
+import com.luizjedi.mongoproject.resources.util.URL;
 import com.luizjedi.mongoproject.services.PostService;
 
 @RestController
@@ -25,4 +29,13 @@ public class PostResources {
 		return ResponseEntity.ok().body(user);
 	}
 
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text") String text) {
+
+		text = URL.decodeParam(text);
+		
+		List<Post> list = service.findByTitle(text);
+
+		return ResponseEntity.ok().body(list);
+	}
 }
