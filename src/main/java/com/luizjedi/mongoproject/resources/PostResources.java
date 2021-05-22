233bb2473da41597371a3033300eaf820c3eb351
luizjedi <luiz.pepe@hotmail.com>
2021-05-21 21:23:14 -0300

    Getting post by id

---
+++ src/main/java/com/luizjedi/mongoproject/resources/PostResources.java
@@ -0,0 +1,28 @@
+package com.luizjedi.mongoproject.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.luizjedi.mongoproject.domain.Post;
+import com.luizjedi.mongoproject.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResources {
+
+	@Autowired
+	private PostService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+
+		Post user = service.findById(id);
+
+		return ResponseEntity.ok().body(user);
+	}
+
+}
---
+++ src/main/java/com/luizjedi/mongoproject/services/PostService.java
@@ -0,0 +1,24 @@
+package com.luizjedi.mongoproject.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luizjedi.mongoproject.domain.Post;
+import com.luizjedi.mongoproject.repository.PostRepository;
+import com.luizjedi.mongoproject.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired // injeção automatica de dependencia.
+	private PostRepository repo;
+
+	public Post findById(String id) {
+
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
